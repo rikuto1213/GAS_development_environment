8390fedec24068f4f29a606945f334e4b6051692
DEMO <show.0417@icloud.com>
2024-11-24 20:16:15 +0900

    初プッシュ

---
+++ esbuild.js
@@ -0,0 +1,15 @@
+import esbuild from "esbuild";
+import { GasPlugin } from "esbuild-gas-plugin";
+
+esbuild
+  .build({
+    entryPoints: ["./src/index.ts"],
+    bundle: true,
+    minify: true,
+    outfile: "./dist/index.js",
+    plugins: [GasPlugin],
+  })
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  });
